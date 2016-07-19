8f28bace91d07cfffb0a9e7b36ffa78d70c9aa2a
IlyaFinkelshteyn <ilyaf@appveyor.com>
2016-07-19 16:32:58 -0700

    Initial upload

---
+++ ProjectA/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectA
+{
+    public class ClassA
+    {
+        public int MyPropertyA { get; set; }
+        
+        public ClassA(int myPropertyA)
+        {
+            this.MyPropertyA = myPropertyA;
+        }
+
+    }
+}
