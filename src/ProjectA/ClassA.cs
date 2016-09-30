87dbbd747ee859504c8281bee9c2b4af54e88fa8
IlyaFinkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2016-09-30 11:34:42 -0700

    Comment to trigger build

--- src/ProjectA/ClassA.cs
+++ src/ProjectA/ClassA.cs
@@ -9,7 +9,7 @@ namespace ProjectA
     public class ClassA
     {
         public int MyPropertyA { get; set; }
-        
+        // super comment
         public ClassA(int myPropertyA)
         {
             this.MyPropertyA = myPropertyA;
