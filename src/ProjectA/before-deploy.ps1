85000bddf158eb84db271ed60af3f9fd2d7a8ac7
IlyaFinkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2016-10-14 15:38:27 -0700

    Update before-deploy.ps1

--- src/ProjectA/before-deploy.ps1
+++ src/ProjectA/before-deploy.ps1
@@ -1,1 +1,7 @@
-
+Write-host "before-deploy.ps1"
+Write-host "APPVEYOR_PROJECT_SLUG: $env:APPVEYOR_PROJECT_SLUG"
+Write-host "APPVEYOR_REPO_COMMIT_AUTHOR: $env:APPVEYOR_REPO_COMMIT_AUTHOR"
+Write-host "APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL: $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL"
+Write-host "myVarA: $env:myVarA"
+Write-host "myVarB: $env:myVarB"
+#throw "failed1"
