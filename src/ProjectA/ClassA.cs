8dd56100a26494503fe522cb46da4a2952823da6
IlyaFinkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2016-10-18 12:39:37 -0700

    Update ClassA.cs

--- src/ProjectA/ClassA.cs
+++ src/ProjectA/ClassA.cs
@@ -9,7 +9,7 @@ namespace ProjectA
     public class ClassA
     {
         public int MyPropertyA { get; set; }
-        // super comment
+        // super comment  112
         public ClassA(int myPropertyA)
         {
             this.MyPropertyA = myPropertyA;
