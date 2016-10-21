a97a0dc05194657c068bbff7dd7e3fd56bd45184
IlyaFinkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2016-10-21 14:04:53 -0700

    pull 44

--- src/ProjectA/ClassA.cs
+++ src/ProjectA/ClassA.cs
@@ -9,7 +9,7 @@ namespace ProjectA
     public class ClassA
     {
         public int MyPropertyA { get; set; }
-        // pull 33
+        // pull 44
         public ClassA(int myPropertyA)
         {
             this.MyPropertyA = myPropertyA;
