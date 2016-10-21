2e278158b5e5276b3f8ffa3881a295601bca6138
IlyaFinkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2016-10-21 14:02:44 -0700

    Update ClassA.cs

--- src/ProjectA/ClassA.cs
+++ src/ProjectA/ClassA.cs
@@ -9,7 +9,7 @@ namespace ProjectA
     public class ClassA
     {
         public int MyPropertyA { get; set; }
-        // pull
+        // pull 33
         public ClassA(int myPropertyA)
         {
             this.MyPropertyA = myPropertyA;
