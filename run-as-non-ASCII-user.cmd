117afddc424ccdc09c8756499306f99b026c2dc8
Ilya Finkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2017-03-27 12:37:59 -0700

    Update run-as-non-ASCII-user.cmd

--- run-as-non-ASCII-user.cmd
+++ run-as-non-ASCII-user.cmd
@@ -1,4 +1,3 @@
-chcp 65001
-net user "илья" password@12 /add
-net localgroup administrators илья /add
-psexec \\localhost -u илья -p password@12 -accepteula ping google.com
+chcp.com 65001
+whoami
+ping google.com
